b991754902726c1350272b3ca8c295c18557c0c2
SabaGPT <128382419+SabaGPT@users.noreply.github.com>
2024-09-02 22:14:42 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import "./assets/main.css";
 import { createApp } from "vue";
-import time1 from '@/components/time1.vue';
-createApp(time1).mount("#app");
+import Time1 from '@/components/time1.vue';
+createApp(Time1).mount("#app");
 
